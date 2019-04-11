01d1cd2d5a630dc2994605ec100fc0a2f6d0fc00
sldstrst <bog7673@gmail.com>
2019-04-11 23:31:33 +0300

    slider images

--- scriptjs/script.js
+++ scriptjs/script.js
@@ -1,7 +1,22 @@
 "use strict"
 
-document.onclick = function(event){
-   if (event.target.tagName == 'IMG'){
-   event.target.classList.add('bordered')
-   }
+var left = -10;
+polosa.style.left = left + 'px';
+
+document.onclick = function (event) {
+  if (event.target.tagName == 'IMG') {
+    event.target.classList.add('bordered');
+  }
+}
+
+document.getElementById('slider-left').onclick = sliderNext;
+
+
+function sliderNext() {
+  var polosa = document.getElementById('polosa');
+  left -= 230;
+  polosa.style.left = left + 'px';
+  if (left <= -230) {
+    left = 450;
+  }
 }
