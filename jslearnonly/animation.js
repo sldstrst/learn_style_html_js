c83f1c156d5c2e60ed9539ba8a37564dc3ad8adb
sldstrst <bog7673@gmail.com>
2019-05-12 01:34:15 +0300

    rest

--- jslearnonly/animation.js
+++ jslearnonly/animation.js
@@ -1,8 +1,32 @@
-function toggleDiv(divID) {
-    $("#" + divID).fadeToggle(1000, function() {
-        if ($(this).is(':visible')){
-            var resultDiv =  divID;
-        }
-    });
-}
+// function toggleDiv(divID) {
+//     $("#" + divID).fadeToggle(800, function() {
+//         if ($(this).is(':visible')){
+//             var resultDiv =  divID;
+//         }
+        
+//     });
 
+//     if ($("#" + divID).is(':visible')){
+
+//     }
+
+
+// }
+
+
+
+$(document).ready(function(divID){
+    $("button").click(2000, function(){
+        if($(this).is(':visible')){
+            $("#" + divID).slideDown("slow");
+        }
+        else
+        $("#" + divID).sl("slow");
+    });
+});
+
+
+$(".btn-slide").click(function(){
+        $("#panel").slideToggle("slow");
+        $(this).toggleClass("active");
+    });
